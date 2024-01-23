7d5d22af4eb81a5ffb78d9cc3bf0a49198506625
XaviMarti89 <125730044+XaviMarti89@users.noreply.github.com>
2024-01-23 08:38:49 +0100

    Add files via upload

--- Manager/js/index.js
+++ Manager/js/index.js
@@ -15,28 +15,39 @@ const eventsListInDB = ref(database, "stats")
 
 //crear variables de partit
 const match = "";
-const playing = [];
+let player,dorsal;
 
 //crear variables de jugadores
-const mariaOrfila = document.getElementById(17);
+const nuria = document.getElementById("6 Nuria");
+const aixa = document.getElementById("7 Aixa");
+const paulaBahamonde = document.getElementById("8 PaulaBahamonde");
+const maribel = document.getElementById("12 Maribel");
+const julia = document.getElementById("13 Julia");
+const alexa = document.getElementById("15 Alexa");
+const mariaOrfila = document.getElementById("17 MariaOrfila");
+const paulaSintes = document.getElementById("18 PaulaSintes");
+const mariaPuerta = document.getElementById("42 MariaPuerta");
+const andrea = document.getElementById("31 Andrea");
 
 //cream ref a elements HTML//
 const buttonStart = document.getElementById("start");
 const matchEl = document.getElementById("match");
-/*const eventsListEl = document.getElementById("events-list");
+const eventsListEl = document.getElementById("events-list");
 const addButton1p = document.getElementById("1p");
-const addButton1pm = document.getElementById("m1p");
+const addButton1pm = document.getElementById("1m");
 const addButton2p = document.getElementById("2p");
-const addButton2pm = document.getElementById("m2p");
+const addButton2pm = document.getElementById("2m");
 const addButton3p = document.getElementById("3p");
-const addButton3pm = document.getElementById("m3p");
+const addButton3pm = document.getElementById("3m");
 const addButtonDR = document.getElementById("DR");
 const addButtonOR = document.getElementById("OR");
 const addButtonA = document.getElementById("Assist");
-const addButtonTO = document.getElementById("TO");*/
+const addButtonTO = document.getElementById("TO");
+const addButtonFault = document.getElementById("FP");
+const addButtonFaultR = document.getElementById("FR");
 
 //recorre elements base de dades//
-/*onValue(eventsListInDB, function (snapshot) {
+onValue(eventsListInDB, function (snapshot) {
 
     if(snapshot.exists()){
     clearListEl();
@@ -48,49 +59,73 @@ const addButtonTO = document.getElementById("TO");*/
 else{
     eventsListEl.innerHTML = "Not items yet..."
 }
-})*/
+})
 
-/*let buttons = [addButton1p,addButton1pm,addButton2p,addButton2pm,addButton3p,addButton3pm,addButtonDR,addButtonOR,addButtonA,addButtonTO]
+let buttons = [addButtonFaultR,addButtonFault,addButton1p,addButton1pm,addButton2p,addButton2pm,addButton3p,addButton3pm,addButtonDR,addButtonOR,addButtonA,addButtonTO]
 //click per afegir elements a base de dades//
 for(let i = 0; i<buttons.length; i++){
 buttons[i].addEventListener("click", function () {
+/*let text;
+switch(this.id){
+    case "3p": text = "3 punts"
+    case "2p": text = "2 punts"
+    case "1p": text = "1 punt"
+    case "OR": text = "Rebot Ofensiu"
+    case "DR": text = "Rebot Defensiu"
+    case "FR": text = "Falta Rebuda"
+    case "FP": text = "Falta Personal"
+    case "3m": text = "Triple Fallat"
+    case "2m": text = "Tir Falat"
+    case "1m": text = "TL Fallat"
+    case "TO": text = "Pèrdua"
+}*/
+
     const eventData = {
-        author: "Xavi",
-        uid: 9,
+        author: player,
+        uid: dorsal,
         eventType: this.id
       };
     push(eventsListInDB, eventData)
 })
-}*/
+}
 
-buttonStart.addEventListener("click", function(){
+
+/*buttonStart.addEventListener("click", function(){
     match = matchEl.value 
-})
+})*/
 
-mariaOrfila.addEventListener("click", function(){
-    console.log("hola")
-    playing.push("Maria Orfila");
-    console.log(playing)
-})
+let players = [nuria, aixa, paulaBahamonde, maribel, julia, alexa, mariaOrfila, paulaSintes, andrea, mariaPuerta]
+for(let i=0; i<players.length;i++){
+    players[i].addEventListener("click", function(){
+        for(let y=0; y<players.length;y++){
+            players[y].className = "player";
+        }
+        players[i].className += " selected";
+        player = players[i].id.split(" ")[1]
+        dorsal = players[i].id.split(" ")[0]
+    })
+}
+
 
 
 //borra llista de la compra (que no surti duplicat)//
-/*function clearListEl() {
+function clearListEl() {
     eventsListEl.innerHTML = "";
-}*/
+}
 
 //afegir element a llista del HTML//
-/*function appendItemToEventListEl(item) {
+function appendItemToEventListEl(item){
 
     let itemData = item[1];
+    let num = itemData.uid
     let eventType = itemData.eventType
     let eventAuthor = itemData.author
 
     let listItem = document.createElement("li");
-    listItem.textContent = eventType+"-"+eventAuthor;
+    listItem.textContent = eventType+" - "+num+" "+eventAuthor;
 
     eventsListEl.append(listItem);
 
-}*/
+}
 
 
