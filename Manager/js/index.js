132695d76dcfb79b6385eedd8b4b95c3604575eb
XaviMarti89 <125730044+XaviMarti89@users.noreply.github.com>
2024-01-26 08:57:12 +0100

    STATS

--- Manager/js/index.js
+++ Manager/js/index.js
@@ -15,7 +15,7 @@ const eventsListInDB = ref(database, "stats")
 
 //crear variables de partit
 const match = "";
-let player,dorsal;
+let player, dorsal;
 
 //crear variables de jugadores
 const nuria = document.getElementById("6 Nuria");
@@ -49,44 +49,30 @@ const addButtonFaultR = document.getElementById("FR");
 //recorre elements base de dades//
 onValue(eventsListInDB, function (snapshot) {
 
-    if(snapshot.exists()){
-    clearListEl();
-    let eventsListarray = Object.entries(snapshot.val())
-    for (let i = 0; i < eventsListarray.length; i++) {
-        appendItemToEventListEl(eventsListarray[i]);
+    if (snapshot.exists()) {
+        clearListEl();
+        let eventsListarray = Object.entries(snapshot.val())
+        for (let i = 0; i < eventsListarray.length; i++) {
+            appendItemToEventListEl(eventsListarray[i]);
+        }
     }
-}
-else{
-    eventsListEl.innerHTML = "Not items yet..."
-}
+    else {
+        eventsListEl.innerHTML = "Not items yet..."
+    }
 })
 
-let buttons = [addButtonFaultR,addButtonFault,addButton1p,addButton1pm,addButton2p,addButton2pm,addButton3p,addButton3pm,addButtonDR,addButtonOR,addButtonA,addButtonTO]
+let buttons = [addButtonFaultR, addButtonFault, addButton1p, addButton1pm, addButton2p, addButton2pm, addButton3p, addButton3pm, addButtonDR, addButtonOR, addButtonA, addButtonTO]
 //click per afegir elements a base de dades//
-for(let i = 0; i<buttons.length; i++){
-buttons[i].addEventListener("click", function () {
-/*let text;
-switch(this.id){
-    case "3p": text = "3 punts"
-    case "2p": text = "2 punts"
-    case "1p": text = "1 punt"
-    case "OR": text = "Rebot Ofensiu"
-    case "DR": text = "Rebot Defensiu"
-    case "FR": text = "Falta Rebuda"
-    case "FP": text = "Falta Personal"
-    case "3m": text = "Triple Fallat"
-    case "2m": text = "Tir Falat"
-    case "1m": text = "TL Fallat"
-    case "TO": text = "Pèrdua"
-}*/
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
 
-    const eventData = {
-        author: player,
-        uid: dorsal,
-        eventType: this.id
-      };
-    push(eventsListInDB, eventData)
-})
+        const eventData = {
+            author: player,
+            uid: dorsal,
+            eventType: this.id
+        };
+        push(eventsListInDB, eventData)
+    })
 }
 
 
@@ -95,9 +81,9 @@ switch(this.id){
 })*/
 
 let players = [nuria, aixa, paulaBahamonde, maribel, julia, alexa, mariaOrfila, paulaSintes, andrea, mariaPuerta]
-for(let i=0; i<players.length;i++){
-    players[i].addEventListener("click", function(){
-        for(let y=0; y<players.length;y++){
+for (let i = 0; i < players.length; i++) {
+    players[i].addEventListener("click", function () {
+        for (let y = 0; y < players.length; y++) {
             players[y].className = "player";
         }
         players[i].className += " selected";
@@ -113,19 +99,151 @@ function clearListEl() {
     eventsListEl.innerHTML = "";
 }
 
+function eventShow(e) {
+    let text;
+    switch (e) {
+        case "3p": text = "3 punts"; break;
+        case "2p": text = "2 punts"; break;
+        case "1p": text = "1 punt"; break;
+        case "OR": text = "Rebot Ofensiu"; break;
+        case "DR": text = "Rebot Defensiu"; break;
+        case "FR": text = "Falta Rebuda"; break;
+        case "FP": text = "Falta Personal"; break;
+        case "3m": text = "Triple Fallat"; break;
+        case "2m": text = "Tir Fallat"; break;
+        case "1m": text = "TL Fallat"; break;
+        case "TO": text = "Pèrdua"; break;
+        case "Assist": text = "Assistència"; break;
+    }
+    return text;
+}
+
 //afegir element a llista del HTML//
-function appendItemToEventListEl(item){
+function appendItemToEventListEl(item) {
 
     let itemData = item[1];
     let num = itemData.uid
     let eventType = itemData.eventType
     let eventAuthor = itemData.author
+    let ev = eventShow(eventType);
+
 
     let listItem = document.createElement("li");
-    listItem.textContent = eventType+" - "+num+" "+eventAuthor;
+    listItem.textContent = num + " " + eventAuthor + " - " + ev;
 
     eventsListEl.append(listItem);
 
 }
 
+
+
+
+let myChart;
+
+document.getElementById("cStats").addEventListener("click", function () {
+
+    onValue(eventsListInDB, function (snapshot) {
+
+        let pT = 0
+        let pT1 = 0
+        let pT2 = 0
+        let pT3 = 0
+        let OR = 0
+        let DR = 0
+        let A = 0
+        let TO = 0
+        let FR = 0
+        let FP = 0;
+        let T1 = 0;
+        let T2 = 0;
+        let T3 = 0
+        let mT1 = 0
+        let mT2 = 0
+        let mT3 = 0;
+
+        if (snapshot.exists()) {
+            let events = Object.entries(snapshot.val())
+
+            for (let i = 0; i < events.length; i++) {
+                switch (events[i][1].eventType) {
+                    case "3p": T3++; break;
+                    case "2p": T2++; break;
+                    case "1p": T1++; break;
+                    case "OR": OR++; break;
+                    case "DR": DR++; break;
+                    case "FR": FR++; break;
+                    case "FP": FP++; break;
+                    case "3m": mT3++; break;
+                    case "2m": mT2++; break;
+                    case "1m": mT1++; break;
+                    case "TO": TO++; break;
+                    case "Assist": A++; break;
+                }
+            }
+
+            console.log(T3+" "+T2+" "+T1+" "+mT3+" "+mT2+" "+mT1)
+
+            pT1 = (T1 / (T1 + mT1)) * 100;
+            pT2 = (T2 / (T2 + mT2)) * 100;
+            pT3 = (T3 / (T3 + mT3)) * 100;
+            pT = ((T2 + T3) / (T2 + mT2 + T3 + mT3)) * 100
+
+            console.log(pT3+" "+pT2+" "+pT1+" "+pT)
+
+
+
+            const labels = ["%T", "%T2", "%T3", "%T1", "OR", "DR", "A", "TO", "FR", "FP"]
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: 'Stats',
+                    data: [pT, pT1, pT2, pT3, OR, DR, A, TO, FR, FP],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
+                    borderWidth: 1
+                }]
+            };
+
+
+            const config = {
+                type: 'bar',
+                data: data,
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                },
+            };
+
+
+            const ctx = document.getElementById('myChart').getContext('2d');
+            if (myChart) {
+                myChart.destroy();
+            }
+
+            myChart = new Chart(ctx, config);
+
+        }
+
+    })
+
+});
 
